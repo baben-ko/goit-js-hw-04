5256246fb3dd45358783083484f763d5f1d0c296
Andrii_Babenko <andrey.brnr@gmail.com>
2020-12-21 22:05:05 +0200

    task-2 DONE

--- js/task-2.js
+++ js/task-2.js
@@ -17,16 +17,16 @@ const inventory = {
 
 const invokeInventoryAction = function(itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  action(itemName);
+  action.call(this, itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction.call(inventory, 'Medkit', inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction.call(inventory, 'Gas mask', inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
